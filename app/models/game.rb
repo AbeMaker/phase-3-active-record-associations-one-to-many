051ab83bde71a3318ead9d72414a51e7517619fa
Abraham Anyang <anyangmaker90@gmail.com>
2022-09-01 13:59:43 +0300

    lab completed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
+    has_many :reviews
   
-end
+  end
+
+  #We need to tell the Game class that each game instance can have many reviews.
+  # We will use the has_many macro to do it:
