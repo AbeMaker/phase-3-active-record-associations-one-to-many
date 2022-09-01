e8735687e3d3d91d3efabc54e2af98b373dd80ac
Abraham Anyang <anyangmaker90@gmail.com>
2022-09-01 13:59:22 +0300

    commit tree 7aa1bffcca4114616d1d318d6f8330f16ef4e053

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,21 @@
 class Review < ActiveRecord::Base
+    belongs_to :game
   
-end
+  end
+
+  #This does the same job as the belongs to in the class
+  #This makes work much easier
+  # belongs_to is a method that is inherited from ActiveRecord::Base that takes an argument of a symbol:
+#   class Review < ActiveRecord::Base
+#     # a review belongs to a game
+#     def game
+#       # self is the review instance
+#       Game.find(self.game_id)
+#     end
+  
+#   end
+
+#We just call the method without parentheses because it looks nicer
+# class Review < ActiveRecord::Base
+#     belongs_to(:game)
+#   end
